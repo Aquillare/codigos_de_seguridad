f30c9004a07d0d4a218303cf15213ee8db96b2f7
Miguel <migueleduagui@gmail.com>
2022-06-20 23:14:29 -0300

    metodos del ciclo de vida en React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,16 +1,45 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            error:false,
+            loading:false,
+        };
+    };
+
+    componentDidUpdate(){
+        if(this.state.loading){
+            setTimeout( () => {
+                this.setState({ loading:false});
+            }, 3000);
+        }
+    }
+
     render(){
         return (
             <div>
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor escribe el código de seguridad</p>
+
+                {this.state.error && (
+                    <p>Error: el código es incorrecto</p>
+                )}
+
+                {this.state.loading && (
+                    <Loading/>
+                )}
+
                 <input placeholder="Código de seguridad"/>
-                <button>Comprobar</button>
+                <button
+                    onClick={() => this.setState({ loading:true})}
+                >Comprobar</button>
             </div>
         );
-    }
-}
+    };
+};
 
 export{ClassState};
